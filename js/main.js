52655bd7cbf7f44bfb0b6f55a66022f2ceccf9e3
kwenox25 <113370944+kwenox25@users.noreply.github.com>
2023-10-25 09:27:40 -0400

    added images on array

--- js/main.js
+++ js/main.js
@@ -16,37 +16,54 @@
     {
       title: "Air Vent",
       desc: "The air vent is a small opening or perforation in the earbud's casing designed to provide ventilation. It helps prevent a vacuum seal in your ear, enhancing comfort during extended use and allowing for better sound quality by equalizing air pressure inside and outside the earbud.",
+      icon: "images/airvent.png",
     },
 
     {
       title: "L for Left Label",
       desc: "The L label on the earbud serves as a clear indication of which ear the earbud should be placed in. This is essential for users to ensure they're wearing the earbuds correctly for optimal audio quality and comfort.",
+      icon: "images/label.png",
     },
 
     {
       title: "Soft Earbud:",
       desc: "Soft earbuds are the part of the earbud that comes into direct contact with the user's ear canal. They are typically made from soft, comfortable materials to ensure a secure and comfortable fit. The choice of soft materials helps reduce discomfort during extended use and enhances noise isolation.",
+      icon: "images/earbuds.png",
     },
 
     {
       title: "Sound Output Hole:",
       desc: "The sound output hole is where audio is emitted from the earbud. It's the point where the music or other audio signals generated within the earbud's driver or speaker are released into the ear canal to be heard by the user.",
+      icon: "images/sound.png",
     },
 
     {
       title: "Magnetic Charging Pin 1:",
       desc: "Magnetic charging pins are connectors used for recharging the earbuds. They are often magnetized to help the earbuds snap into place on a charging dock or case, ensuring a secure and reliable connection for charging. Magnetic Charging Pin 1 implies it's one of the two connectors on the earbud.",
+      icon: "images/charge.png",
     },
 
     {
       title: "Magnetic Charging Pin 2:",
       desc: "Similar to Magnetic Charging Pin 1, this is the second connector used for charging the earbud. Having two pins often allows for faster charging and more secure connection between the earbud and the charging dock or case.",
+      icon: "images/charge.png",
     },
 
     {
       title: "Microphone Hole:",
       desc: "The microphone hole is where the built-in microphone is located on the earbud. This is essential for voice calls, voice commands, or any other feature that requires audio input from the user. The hole allows sound to reach the microphone for clear communication.",
+      icon: "images/microphone.png",
     },
+  ];
+
+  const imagePaths = [
+    "images/airvent.png",
+    "images/label.png",
+    "images/earbuds.png",
+    "images/sound.png",
+    "images/charge.png",
+    "images/charge.png",
+    "images/microphone.png",
   ];
 
   function showInfo(e) {
@@ -64,11 +81,14 @@
 
     const titleElement = selected.querySelector("h2");
     const descElement = selected.querySelector("p");
+    const imgElement = selected.querySelector(".hotspot-image");
 
     const infoHotspot = infoHotspots[hotspotIndex]; // Get the hotspot info
 
     titleElement.textContent = infoHotspot.title;
     descElement.textContent = infoHotspot.desc;
+
+    imgElement.src = imagePaths[hotspotIndex];
 
     gsap.to(selected, 1, { autoAlpha: 1 });
   }
