aea75ef4363e3cf218ef5d186224857ac57a3b0b
kwenox25 <113370944+kwenox25@users.noreply.github.com>
2023-11-04 01:35:16 -0400

    added slider and footer

--- js/main.js
+++ js/main.js
@@ -137,3 +137,45 @@ function changeBGImage(event) {
 buttonColours.forEach((button) => {
   button.addEventListener("click", changeBGImage);
 });
+
+// xray slider
+(() => {
+  let imageCon = document.querySelector("#imageCon"),
+    drag = document.querySelector(".image-drag"),
+    left = document.querySelector(".image-left"),
+    dragging = false,
+    min = 0,
+    max = imageCon.offsetWidth;
+
+  function onDown() {
+    dragging = true;
+    console.log("Set to true");
+  }
+
+  function onUp() {
+    dragging = false;
+    console.log("Set to false");
+  }
+
+  function onMove(event) {
+    // console.log("on move called");
+    if (dragging === true) {
+      //   console.log("dragging");
+      let x = event.clientX - imageCon.getBoundingClientRect().left;
+      console.log(x);
+
+      if (x < min) {
+        x = min;
+      } else if (x > max) {
+        x = max - 10;
+      }
+
+      drag.style.left = x + "px";
+      left.style.width = x + "px";
+    }
+  }
+
+  drag.addEventListener("mousedown", onDown);
+  document.body.addEventListener("mouseup", onUp);
+  document.body.addEventListener("mousemove", onMove);
+})();
