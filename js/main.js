6860392d705db442f591e555c12c9e2829d07ab6
kwenox25 <113370944+kwenox25@users.noreply.github.com>
2023-11-07 23:28:35 -0500

    added animation of my earbuds

--- js/main.js
+++ js/main.js
@@ -166,3 +166,56 @@ buttonColours.forEach((button) => {
   document.body.addEventListener("mouseup", onUp);
   document.body.addEventListener("mousemove", onMove);
 })();
+
+(() => {
+  const canvas = document.querySelector("#explode-view");
+  const context = canvas.getContext("2d");
+
+  canvas.width = 1920;
+  canvas.height = 1080;
+  const frameCount = 700; // number of frames we have
+  const images = []; // an array to hold all of our images
+
+  const buds = {
+    frame: 0,
+  };
+
+  for (let i = 0; i < frameCount; i++) {
+    // console.log(i);
+    // const img = new Image();
+    const img = document.createElement("img");
+
+    // need to recreate a string: images/explode_0001.webp
+
+    img.src = `images/animation/animate_${(i + 1)
+      .toString()
+      .padStart(4, "0")}.jpg`;
+    images.push(img);
+  }
+
+  //   console.table(images);
+  // not animating a DOM element, but rather an object
+  // which contains a frame count
+  gsap.to(buds, {
+    frame: 699,
+    snap: "frame",
+    scrollTrigger: {
+      trigger: "#explode-view",
+      pin: true,
+      scrub: 1,
+      start: "top top",
+      markers: true,
+    },
+
+    onUpdate: render,
+  });
+
+  images[0].addEventListener("onload", render());
+
+  function render() {
+    // console.log(buds.frame);
+    // console.log(images[buds.frame]);
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(images[buds.frame], 0, 0);
+  }
+})();
